ed2a08e3cc916f6f88494c950e0461bd33ed2b32
Gibson <d.evsyukov02@gmail.com>
2023-11-19 14:58:08 +0300

    Finish 6 lab

--- Lab6/client/main.c
+++ Lab6/client/main.c
@@ -2,9 +2,6 @@
 #include "lib/include/client.h"
 int main(int argc, char* argv[]){
     //StartClientContext("127.0.0.1","10102","bye");
-    char* s[] = {"","1","localhost", "10102"};
-    StartClientContextArgs(sizeof(s) / sizeof(s[0]), s);
-    return 0;
     void (*fptrs_args[])(int, char* []) = {NULL, StartClientContextArgs, StartClientPipeContextArgs};
     int res = 0;
     int size = sizeof(fptrs_args) / sizeof(fptrs_args[1]);
